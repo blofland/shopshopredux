59be688d36e41630437bc9f8f728bfae91ad7d46
unknown <boedi15@gmail.com>
2021-12-10 22:57:49 -0700

    Refactored CartItem for Redux

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+// import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch } from 'react-redux';
+// import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../store/ui/ui';
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
-  const [, dispatch] = useStoreContext();
+  // const [, dispatch] = useStoreContext();
+  const dispatch = useDispatch()
 
   const removeFromCart = item => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: item._id
-    });
+    // dispatch({
+    //   type: REMOVE_FROM_CART,
+    //   _id: item._id
+    // });
+    dispatch(REMOVE_FROM_CART({_id: item._id}))
     idbPromise('cart', 'delete', { ...item });
 
   };
@@ -19,10 +23,11 @@ const CartItem = ({ item }) => {
   const onChange = (e) => {
     const value = e.target.value;
     if (value === '0') {
-      dispatch({
-        type: REMOVE_FROM_CART,
-        _id: item._id
-      });
+      // dispatch({
+      //   type: REMOVE_FROM_CART,
+      //   _id: item._id
+      // });
+      dispatch(REMOVE_FROM_CART({_id: item._id}))
       idbPromise('cart', 'delete', { ...item });
 
     } else {
@@ -31,6 +36,7 @@ const CartItem = ({ item }) => {
         _id: item._id,
         purchaseQuantity: parseInt(value)
       });
+      dispatch(UPDATE_CART_QUANTITY({_id: item._id, purchaseQuantity: parseInt(value)}))
       idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
 
     }
