41da8d941ecdf91d1fcf3aaa7229457e2ac7f64a
unknown <boedi15@gmail.com>
2021-12-10 23:10:29 -0700

    Updated ProductItem for redux

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+// import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch, useSelector } from "react-redux";
+// import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../store/ui/ui";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch()
+  const cart = useSelector(s => s.ui.cart)
 
   const {
     image,
@@ -16,25 +20,29 @@ function ProductItem(item) {
     quantity
   } = item;
 
-  const { cart } = state
+  // const { cart } = state
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
     if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: _id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      });
+      // dispatch({
+      //   type: UPDATE_CART_QUANTITY,
+      //   _id: _id,
+      //   purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      // });
+      dispatch(UPDATE_CART_QUANTITY({
+        _id, purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      }))
       idbPromise('cart', 'put', {
         ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
-      });
+      // dispatch({
+      //   type: ADD_TO_CART,
+      //   product: { ...item, purchaseQuantity: 1 }
+      // });
+      dispatch(ADD_TO_CART({ ...item, purchaseQuantity: 1 }))
       idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
   }
