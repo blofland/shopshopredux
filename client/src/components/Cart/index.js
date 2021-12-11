37194dad847a09b4919b157713d16308cfe74262
unknown <boedi15@gmail.com>
2021-12-10 22:54:44 -0700

    Refactored Cart for redux

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,14 +5,17 @@ import { QUERY_CHECKOUT } from "../../utils/queries"
 import { idbPromise } from "../../utils/helpers"
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
-import { useStoreContext } from "../../utils/GlobalState";
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+// import { useStoreContext } from "../../utils/GlobalState";
+// import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../store/ui/ui";
+import { useDispatch, useSelector } from "react-redux";
 import "./style.css";
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch()
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
@@ -26,7 +29,8 @@ const Cart = () => {
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
-      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+      dispatch(ADD_MULTIPLE_TO_CART(cart))
+      // dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
     };
 
     if (!state.cart.length) {
@@ -35,7 +39,9 @@ const Cart = () => {
   }, [state.cart.length, dispatch]);
 
   function toggleCart() {
-    dispatch({ type: TOGGLE_CART });
+    // dispatch({ type: TOGGLE_CART });
+    dispatch(TOGGLE_CART())
+
   }
 
   function calculateTotal() {
